006ee96c89eb8e2af5ba8ef887447a90aa274618
Leandro Liset <leandroliset@gmail.com>
2024-08-24 15:06:48 -0300

    Modificado el carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ItemsContext } from "../contexts/ItemContext";
 import cart from "../assets/cart.webp";
 
-
 export const CartWidget = () => {
+    const { items } = useContext(ItemsContext);
+
+    const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
+
     return (
         <Link to="/cart">
-            <img src={cart} height={25} /><span>3</span> 
+            <img src={cart} height={25} alt="Carrito" />
+            {totalItems > 0 && <span>{totalItems}</span>}
         </Link>
     );
 };
